4e9eb26ea00bde56e34221412a9cd010340d23ba
Mar Orpianesi <orpianesi.mar@gmail.com>
2021-04-05 13:39:53 -0300

    first commmit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,60 @@
+package org.example;
+
+
+import java.util.Scanner;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Time!" );
+        int check = 0;
+        Scanner s = new Scanner(System.in);
+        Time time = new Time();
+
+        do {
+            System.out.print("Ingrese la hora: ");
+            int hour = s.nextInt();
+            if (hour >= 0 && hour <= 23) {
+                time.setHour(hour);
+                check = 0;
+            } else {
+                System.out.println("La hora introducida no es valida!");
+                check = 1;
+            }
+        }while(check == 1);
+
+        do {
+            System.out.print("Ingrese los minutos: ");
+            int minute = s.nextInt();
+            if (minute >= 0 && minute <= 59) {
+                time.setMinutes(minute);
+                check = 0;
+            } else {
+                System.out.println("Los minutos introducidos no son válidos!");
+                check = 1;
+            }
+        }while(check == 1);
+
+        do {
+            System.out.print("Ingrese los segundos: ");
+            int second = s.nextInt();
+            if (second >= 0 && second <= 59) {
+                time.setSeconds(second);
+                check = 0;
+            } else {
+                System.out.println("Los segundos introducidos no son válidos");
+                check = 1;
+            }
+        }while(check == 1);
+
+        System.out.println(time.verDetalle());
+        time.retrocederSegundo();
+        System.out.println("-1 SEGUNDO");
+        System.out.println(time.verDetalle());
+        time.avanzarSegundo();
+        System.out.println("+1 SEGUNDO");
+        System.out.println(time.verDetalle());
+
+    }
+}
---
+++ src/main/java/org/example/Time.java
@@ -0,0 +1,61 @@
+package org.example;
+
+public class Time {
+    private int hour;
+    private int minutes;
+    private int seconds;
+
+    /*****************************GETTERS AND SETTERS****************************************/
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public int getMinutes() {
+        return minutes;
+    }
+
+    public void setMinutes(int minutes) {
+        this.minutes = minutes;
+    }
+
+    public int getSeconds() {
+        return seconds;
+    }
+
+    public void setSeconds(int seconds) {
+        this.seconds = seconds;
+    }
+
+    /*******************************METODOS********************************************/
+    public String verDetalle()
+    {
+         StringBuilder sb = new StringBuilder();
+
+         if(hour < 10){
+             sb.append("0");
+         }
+         sb.append(this.hour).append(":");
+         if(minutes < 10) {
+             sb.append("0");
+         }
+         sb.append(this.minutes).append(":");
+         if(seconds < 10){
+             sb.append("0");
+         }
+         sb.append(this.seconds);
+    return sb.toString();
+    }
+
+    public int avanzarSegundo(){
+        return this.seconds +=1;
+    }
+    public int retrocederSegundo(){
+        return this.seconds -=1;
+    }
+
+
+}
